0063b1165ee092310cfbe0f19f118fb9ea970586
yusifhasanov <yusif.hasanov17@gmail.com>
2025-02-28 15:38:54 +0400

    dsadasdadasasdadsadadasda

--- public/assets/lang-config.js
+++ public/assets/lang-config.js
@@ -2,8 +2,8 @@ window.__GOOGLE_TRANSLATION_CONFIG__ = {
     languages: [
         { title: 'English', name: 'en' },
         { title: 'Deutsch', name: 'de' },
-        { title: 'Русский', name: 'ru' },
-        { title: 'Azərbaycan', name: 'az' },
+        // { title: 'Русский', name: 'ru' },
+        // { title: 'Azərbaycan', name: 'az' },
     ],
     defaultLanguage: 'de',
 };
