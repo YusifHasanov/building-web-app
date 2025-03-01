a631368c5dcd7ff5d18bcae8a19eb92465fe9457
yusifhasanov <yusif.hasanov17@gmail.com>
2025-03-02 00:19:01 +0400

    dsadasdadasasdadsadadasda

--- components/footer.tsx
+++ components/footer.tsx
@@ -90,6 +90,7 @@ export function Footer() {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
           <p>&copy; {new Date().getFullYear()} Baku Bau GmbH. Alle Rechte vorbehalten.</p>
+          <p>&copy;  Site by Jeykhun Imanov Studio</p>
         </div>
       </div>
     </footer>
