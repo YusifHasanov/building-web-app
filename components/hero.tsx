8378eabd206065064ef691d59e98ca0dcd81a7ec
yusifhasanov <yusif.hasanov17@gmail.com>
2025-03-01 23:36:04 +0400

    dsadasdadasasdadsadadasda

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,8 +8,10 @@ import { Button } from "@/components/ui/button"
 
 const backgroundImages = [
     "/HomeBg.png",
-    "https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?auto=compress&cs=tinysrgb&w=1920",
-    "https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=compress&cs=tinysrgb&w=1920",
+   "/slide1.webp",
+   "/slide2.jpg",
+   "/slide3.jpg",
+   "/slide4.jpg",
 ]
 
 export function Hero() {
